fb80c0cd65a14ae83737a820a3e3d8438095e1a5
Adejair Junior <adejair.junior@outlook.com>
2017-06-25 12:47:45 -0400

    Adicionado um exemplo para trabalhar com as classes :art:

--- Menu.java
+++ Menu.java
@@ -2,5 +2,15 @@
 *
 */
 
+import java.utils.ArrayList;
+
 class Menu {
+  public ArrayList<Clientes> controllerClientes = new ArrayList<Clientes>();
+
+  /**
+  * Menu Cadastro
+  * - Selecionou Cadastrar Cliente
+  * - Clientes novoCliente = new Clientes().cadastrarCliente(..);
+  * - controllerClientes.add(novoCliente);
+  */
 }
